6ed5ad5a4ecef3fdd3ad38e8c7d50a17821d6552
xiazhongwei <xiazhongwei@able-elec.com>
2017-09-04 16:47:38 +0800

    springBoot

--- Spring Boot基础教程2-RESTfull API简单项目的快速搭建/spring-boot-demo-2-1/spring-boot-demo-2-1/src/main/java/com/roncoo/education/SpringBootDemo21Application.java
+++ Spring Boot基础教程2-RESTfull API简单项目的快速搭建/spring-boot-demo-2-1/spring-boot-demo-2-1/src/main/java/com/roncoo/education/SpringBootDemo21Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootDemo21Application {
 
+	// main
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDemo21Application.class, args);
 	}
